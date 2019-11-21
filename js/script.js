1c2a628665f8327702a6ad4d571b2fe35fddf910
seaster93 <54690978+seaster93@users.noreply.github.com>
2019-11-21 14:27:41 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-function init(){
-//add your javascrip between these two lines of code
- document.getElementById("entrybutton").addEventListener("click", output);
- 
- document.getElementById("entrybutton").addEventListener("click", Alert);
-  } 
-    function Alert(){
-        alert("Sydney Easter: " + document.getElementById("entryinput").value);
-    }
-    function output(){
-        document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
-    }
 
-
-window.addEventListener('load', init);
